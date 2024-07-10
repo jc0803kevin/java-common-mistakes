a8caf8bff301a350dca20a16b5854dcd25c61627
肖拔赟 <bayun.xiao@vonechain.com>
2024-07-10 18:47:46 +0800

    feat:mat

---
+++ troubleshootingtools/mat/src/main/java/icu/kevin/troubleshootingtools/mat/FooService.java
@@ -0,0 +1,20 @@
+package icu.kevin.troubleshootingtools.mat;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Component
+public class FooService {
+    List<String> data = new ArrayList<>();
+
+    public void oom() {
+        // 加入大小为10KB的字符串
+        data.add(IntStream.rangeClosed(1, 10_000)
+                .mapToObj(__ -> "a")
+                .collect(Collectors.joining("")));
+    }
+}
